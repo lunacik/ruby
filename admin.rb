4e06b02e009a3daeae83f2a74d3867f6dda72aa6
lunacik <lunacik1@yahoo.pl>
2012-03-25 13:56:13 +0300

    clear

--- admin.rb
+++
@@ -1,8 +0,0 @@
-
-require './worker'
-
-class Admin < Worker
-  def initialize(name, surname, birthday, pin, address, sin, salary=3000)
-    super(name, surname, birthday, pin, address, sin, salary)
-  end
-end
--- supervisor.rb
+++
@@ -1,9 +0,0 @@
-
-require './worker'
-
-class Supervisor < Worker
-  def initialize(name, surname, birthday, pin, address, sin, salary=2000)
-    super(name, surname, birthday, pin, address, sin, salary)
-  end
-end
-
